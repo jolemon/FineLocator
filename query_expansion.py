e32f30bcead233e6b5979bd7173fd4e00efcc2fc
chinalienming <728385437@qq.com>
2019-10-04 20:23:01 +0800

    init framework and part:word2vec

---
+++ query_expansion.py
@@ -0,0 +1,120 @@
+import math
+import numpy as np
+
+alpha = 0.8
+beta = 0.1
+gamma = 0.1
+
+avg_time_difference = 0
+average_shortest_length = 0
+
+
+# TODO 统计word在词袋bow中出现的次数
+def cal_word_frequency(word, bow):
+    return 0
+
+
+# TODO |M| : 代表项目中所有的方法数
+def cal_num_of_methods():
+    return 0
+
+
+# TODO 统计项目中包含word的方法数
+def cal_num_of_methods_containing_word(word):
+    return 0
+
+
+# The term frequency
+def tf(word, bow):
+    word_frequency = cal_word_frequency(word = word, bow = bow)
+    return math.log(word_frequency) + 1
+
+
+# the inverse document frequency
+def idf(word):
+    big_m = cal_num_of_methods()
+    n_wj = cal_num_of_methods_containing_word(word)
+    return math.log(big_m / n_wj)
+
+
+def cal_tfidf(word, bow):
+    return tf(word, bow) * idf(word)
+
+
+# TODO
+def cal_vector_multiply_tfidf(word, vector_for_word, bow):
+    tfidf_for_word = cal_tfidf(word, bow)
+    # vector_for_word是什么类型，如何乘以一个浮点数
+    # vector_for_word.multiply(tfidf_for_word)
+    return 0
+
+
+# TODO
+def max_pooling_for_bow(vec_tfidf_list):
+    return
+
+
+# TODO 在方法级别上计算doc
+def cal_doc_vector():
+    # for all word in method : do cal_vector_multiply_tfidf() and get vec_tfidf_list
+    # return max_pooling_for_bow(vec_tfidf_list)
+    return
+
+
+# TODO 正确性
+def cal_cosine_similarity(vec_1, vec_2):
+    vector_1 = np.mat(vec_1)
+    vector_2 = np.mat(vec_2)
+    num = float(vector_1 * vector_2.T)
+    denom = np.linalg.norm(vector_1) * np.linalg.norm(vector_1)
+    cos = num / denom
+    return cos
+
+
+# sigmoid函数
+def sigmoid(x):
+    s = 1 / (1 + np.exp(-x))
+    return s
+
+
+# TODO 计算方法之间的平均修改长度
+def cal_average_time_difference():
+    return 0
+
+
+def semantic_similarity(doc_1, doc_2):
+    return cal_cosine_similarity(vec_1 = doc_1, vec_2 = doc_2)
+
+
+def temporal_proximity(last_modified_time_for_m1, last_modified_time_for_m2):
+    time_diff = abs(last_modified_time_for_m1 - last_modified_time_for_m2)
+    return sigmoid(time_diff / avg_time_difference)
+
+
+def cal_average_shortest_length():
+    return 0
+
+
+# TODO JavaUnderstand. 两个方法之间是否有依赖关系，无则返回0，有则返回最短调用长度lp
+def find_link_for_methods(method_1, method_2):
+    return 0
+
+
+def call_dependency(method_1, method_2):
+    lp = find_link_for_methods(method_1 = method_1, method_2 = method_2)
+    if lp == 0:
+        return 0
+    else:
+        return sigmoid(1-lp/average_shortest_length)
+
+
+# TODO 是否需要定义类表示method，method包含其doc_vector, last_modify_time, 但还要另外计算call_dependency
+def method_augmentation(method_1, method_2):
+    # return alpha * semantic_similarity() + beta * temporal_proximity() + gamma * call_dependency()
+    return
+
+if __name__ == '__main__':
+    # 定义 alpha, beta, gamma
+
+    avg_time_difference = cal_average_time_difference()
+    average_shortest_length = cal_average_shortest_length()
---
+++ word2vec/src/main/java/model/Word2VecModel.java
@@ -0,0 +1,76 @@
+package model;
+
+import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
+import org.deeplearning4j.models.word2vec.Word2Vec;
+import org.deeplearning4j.text.sentenceiterator.BasicLineIterator;
+import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
+import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+public class Word2VecModel {
+
+    private static Logger log = LoggerFactory.getLogger(Word2VecModel.class);
+
+    public static String dataLocalPath;
+
+
+    public static void main(String[] args) throws Exception {
+
+        dataLocalPath = DownloaderUtility.NLPDATA.Download();
+        // Gets Path to Text file
+        String filePath = new File(dataLocalPath,"raw_sentences.txt").getAbsolutePath();
+
+        log.info("Load & Vectorize Sentences....");
+        // Strip white space before and after for each line
+        SentenceIterator iter = new BasicLineIterator(filePath);
+        // Split on white spaces in the line to get words
+        TokenizerFactory t = new DefaultTokenizerFactory();
+
+        /*
+            CommonPreprocessor will apply the following regex to each token: [\d\.:,"'\(\)\[\]|/?!;]+
+            So, effectively all numbers, punctuation symbols and some special symbols are stripped off.
+            Additionally it forces lower case for all tokens.
+         */
+        t.setTokenPreProcessor(new CommonPreprocessor());
+
+        log.info("Building model....");
+        Word2Vec vec = new Word2Vec.Builder()
+                .minWordFrequency(5)
+                .iterations(1)
+                .layerSize(100)
+                .seed(42)
+                .windowSize(5)
+                .iterate(iter)
+                .tokenizerFactory(t)
+                .build();
+
+        log.info("Fitting Word2Vec model....");
+        vec.fit();
+
+        log.info("Writing word vectors to text file....");
+        /*
+            at this moment we're supposed to have model built, and it can be saved for future use.
+         */
+        WordVectorSerializer.writeWord2VecModel(vec, "pathToSaveModel.txt");
+
+        /*
+            Let's assume that some time passed, and now we have new corpus to be used to weights update.
+            Instead of building new model over joint corpus, we can use weights update mode.
+         */
+        Word2Vec word2Vec = WordVectorSerializer.readWord2VecModel("pathToSaveModel.txt");
+
+        INDArray wordVectorMatrix = word2Vec.getWordVectorMatrix("day");
+        double[] wordVector = word2Vec.getWordVector("day");
+        System.out.println(wordVectorMatrix);
+        for(double d:wordVector){
+            System.out.print(d+" ");
+        }
+
+    }
+}
