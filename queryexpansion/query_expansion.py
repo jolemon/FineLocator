8f4d2abc922263f7c6a7fc0da667b17c9a64753b
chinalienming <728385437@qq.com>
2019-11-02 17:52:18 +0800

    calculate cosine similarity and rank

--- queryexpansion/query_expansion.py
+++ queryexpansion/query_expansion.py
@@ -5,64 +5,13 @@ from handle_cd_method import trim_method
 import os
 from math_tool import average
 from ss import load_cv
+import numpy as np
 
 alpha = 0.8
 beta = 0.1
 gamma = 0.1
 
 
-# TODO 计算平均值
-def cal_average_augmentation_coefficient():
-    return 0
-
-
-# ss, tp key = ${method1}"#"${method2}, cd key = turple(method1, method2)
-def method_augmentation(ss_path, tp_path, cd_path, cv_path, save_path):
-    ac_dic = dict()
-    with open(ss_path, 'r') as ss_file, open(tp_path, 'r') as tp_file, open(cd_path, 'r') as cd_file:
-        ss_dic = json.loads(ss_file.read())
-        tp_dic = json.loads(tp_file.read())
-        cd_dic = json.loads(cd_file.read())
-        print("load ss, tp, cd dictionary ready.")
-        for tp_key in tp_dic:
-            tp_value = tp_dic[tp_key]
-            ss_value = find_v_by_sharp_k(tp_key, ss_dic)
-            if ss_value is None:
-                continue
-            cd_value = find_v_by_sharp_k(tp_key, cd_dic)
-            if cd_value is None:
-                cd_value = 0
-            ac_value = alpha * ss_value + beta * tp_value + gamma * cd_value
-            ac_dic[tp_key] = ac_value
-
-        del tp_dic
-        del cd_dic
-        avglist = ac_dic.values()
-        avg_ac = average(avglist, len(avglist))
-        print("average augmentation coefficient is", avg_ac)
-        with open(save_path, 'w') as save_file:
-            save_file.write(json.dumps(ac_dic))
-
-        # method augmentation.
-        # methods_dic = load_cv(cv_path)
-        # for method in methods_dic:
-        #     doc_vector = methods_dic[method]
-        #     for ac_key in ac_dic:
-        #         if method in ac_key and ac_dic[ac_key] > avg_ac:
-        #             another_method = ac_key.replace(method, "").replace("#", "")
-        #             try:
-        #                 another_doc = methods_dic[another_method]
-        #             except KeyError:
-        #                 print(another_method, "not found in methods dic.")
-        #                 continue
-        #             doc_vector = doc_vector + another_doc * ac_dic[ac_key]
-        #             methods_dic[method] = doc_vector
-        # with open(save_path, 'w') as save_file:
-        #     save_file.write(json.dumps(methods_dic))
-    return
-
-
-# key = m1#m2
 def find_v_by_sharp_k(key, dic, flag = True):
     if key in dic:
         return dic[key]
@@ -75,7 +24,7 @@ def find_v_by_sharp_k(key, dic, flag = True):
             return dic[switch_key]
         else:
             if flag is True:
-                # step 1 : remove "@* " and function modifier
+                # step 1 : remove "@* " and function modifier and "throws *"
                 trim_m0 = trim_method(m0)
                 trim_m1 = trim_method(m1)
                 # step 2 : try to find new key in dic again
@@ -83,6 +32,65 @@ def find_v_by_sharp_k(key, dic, flag = True):
                 return find_v_by_sharp_k(new_key, dic, flag = False)
             else:
                 return None
+
+
+def calculate_ac(ss_path, tp_path, cd_path):
+    ac_dic = dict()
+    with open(ss_path, 'r') as ss_file:
+        ss_dic = json.loads(ss_file.read())
+    with open(tp_path, 'r') as tp_file:
+        tp_dic = json.loads(tp_file.read())
+    with open(cd_path, 'r') as cd_file:
+        cd_dic = json.loads(cd_file.read())
+
+    print("load ss, tp, cd dictionary ready.")
+    for tp_key in tp_dic:
+        tp_value = tp_dic[tp_key]
+        ss_value = find_v_by_sharp_k(tp_key, ss_dic)
+        if ss_value is None:
+            continue
+        cd_value = find_v_by_sharp_k(tp_key, cd_dic)
+        if cd_value is None:
+            cd_value = 0
+
+        ac_value = alpha * ss_value + beta * tp_value + gamma * cd_value
+        ac_dic[tp_key] = ac_value
+
+    del ss_dic, tp_dic, cd_dic
+    print("ac size:", len(ac_dic))
+    avglist = ac_dic.values()
+    avg_ac = average(avglist, len(avglist))
+    print("average augmentation coefficient is", avg_ac)
+
+    # before method augmentation, filter ac that is lower than avg_ac.
+    for ac_key in ac_dic:
+        if ac_dic[ac_key] < avg_ac:
+            del ac_dic[ac_key]
+    return ac_dic
+
+
+def method_augmentation(cv_path, ac_dic, save_path):
+    methods_dic = load_cv(cv_path)
+    for ac_key in methods_dic:
+        ac_key.split()
+    for method in methods_dic:
+        doc_vector = methods_dic[method]
+        for ac_key in ac_dic:
+            if method in ac_key:
+                another_method = ac_key.replace(method, "").replace("#", "")
+                print(another_method)
+                try:
+                    another_doc = methods_dic[another_method]
+                except KeyError:
+                    print(another_method, "not found in methods dic.")
+                    continue
+
+                doc_vector = doc_vector + another_doc * ac_dic[ac_key]
+                methods_dic[method] = doc_vector
+    with open(save_path, 'w') as save_file:
+        save_file.write(json.dumps(methods_dic))
+    return
+
 
 
 
@@ -103,8 +111,9 @@ if __name__ == '__main__':
 
     start = time.process_time()
     print("Finally, Start to Calculate Query Expansion...")
-    method_augmentation(ss_path = ss_path, tp_path = tp_path, cd_path = cd_path,
-                        cv_path = code_vector_dir, save_path = save_path)
+    ac_dic = calculate_ac(ss_path = ss_path, tp_path = tp_path, cd_path = cd_path)
+    # method_augmentation(cv_path = code_vector_dir, ac_dic = ac_dic, save_path = save_path)
+
     elapsed = round(time.process_time() - start, 2)
     print("Finished Calculate Query Expansion. Time used : ", elapsed, "s.")
     print("File size is around : ", str(round(os.path.getsize(save_path) / (1024 * 1024 * 1024), 2)), "G.")
--- queryexpansion/rank.py
+++ queryexpansion/rank.py
@@ -2,7 +2,50 @@ import math_tool
 
 
 def cal_rel(bug_report_vector, augmented_method_vector):
-    return math_tool.cal_cosine_similarity(bug_report_vector, augmented_method_vector)
+    return math_tool.cosine_similarity(bug_report_vector, augmented_method_vector)
+
+
+def load_augmentation_methods(path):
+    return
+
+
+# read "link_buggyMethods/xxx" file and return a dictionary
+def load_link_dic(path, parent_name_for_path):
+    dic = {}
+    f = open(path, 'r')
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        if len(line) > 0:
+            parts = line.split('\t')
+            br_id = parts[0]
+            pm_lists_str = parts[1].rstrip('\n')
+            pm_lists = pm_lists_str.split('外')
+            for pm_list in pm_lists:
+                pm_parts = pm_list.split('内')
+                if len(pm_parts) < 2:
+                    print(pm_parts, "in this sentence can not find PATH / METHOD_NAME !")
+                    continue
+                path = parent_name_for_path + '/' + str(pm_parts[0])
+                method = pm_parts[1]
+
+                method_parts = method.split(' ')
+                ret_type = method_parts[0]
+                if ret_type.endswith('[]'):
+                    new_method = ret_type[:-2] + ' '
+                    for i in range(1, len(method_parts)):
+                        new_method += method_parts[i] + ' '
+                    new_method = new_method.strip()
+                    path_method = path + '#' + new_method
+                else:
+                    path_method = path + '#' + method
+
+                if br_id not in dic:
+                    dic.setdefault(br_id, []).append(path_method)
+                else:
+                    dic[br_id].append(path_method)
+    return dic
 
 
 # 输出格式与iBug项目一致，则可以复用iBug计算TopK、MAP、MRR的代码
