02155dacc3cd708436385b8640865d15f04494ef
chinalienming <728385437@qq.com>
2020-01-14 17:34:09 +0800

    debug

--- batch_run_cal_res.sh
+++ batch_run_cal_res.sh
@@ -2,7 +2,8 @@
 proj=$1 
 dim=$2         #300
 epochs=$3      #10 
-final_dir=$4   #$5
+finalRootDir=~/en/FineLocator/expRes/final   
+predictResDir=~/en/FineLocator/expRes/res
 
 for((a=1;a<=10;a+=1))
 do
@@ -10,10 +11,10 @@ do
     do
         r=$((10-$a-$b))
         if [[ $r -ge 0 ]]; then          
-            echo "calculate MAP, MRR, Hit-K for ${proj}_${word2vec_model_dimension}_${word2vec_model_epochs}_${a}${b}${r}..."
+            echo "calculate MAP, MRR, Hit-K for ${proj}_${dim}_${epochs}_${a}${b}${r}..."
             ./cal_HitK-MAP-MRR.sh $proj ${word2vec_model_dimension} ${word2vec_model_epochs} $a$b$r ${finalRootDir}
             mkdir -p ${predictResDir}/${proj}
-            mv ${proj}_${word2vec_model_dimension}_${word2vec_model_epochs}_${a}${b}${r}_res ${predictResDir}/${proj}
+            mv ${proj}_${dim}_${epochs}_${a}${b}${r}_res ${predictResDir}/${proj}/
         fi   
     done
 done
