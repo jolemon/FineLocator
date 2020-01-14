43f53f7f90cb1672c7a58f307d4c8bdc6226d187
chinalienming <728385437@qq.com>
2020-01-14 17:26:51 +0800

    batch_run_cal_res

---
+++ batch_run_cal_res.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+proj=$1 
+dim=$2         #300
+epochs=$3      #10 
+final_dir=$5   #$5
+
+for((a=1;a<=10;a+=1))
+do
+    for((b=1;b<=10-$a;b+=1))
+    do
+        r=$((10-$a-$b))
+        if [[ $r -ge 0 ]]; then          
+            echo "calculate MAP, MRR, Hit-K for ${proj}_${word2vec_model_dimension}_${word2vec_model_epochs}_${a}${b}${r}..."
+            ./cal_HitK-MAP-MRR.sh $proj ${word2vec_model_dimension} ${word2vec_model_epochs} $a$b$r ${finalRootDir}
+            mkdir -p ${predictResDir}/${proj}
+            mv ${proj}_${word2vec_model_dimension}_${word2vec_model_epochs}_${a}${b}${r}_res ${predictResDir}/${proj}
+        fi   
+    done
+done
