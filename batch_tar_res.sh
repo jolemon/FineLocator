b5b751ee59bc70891bba975547c320fe8a1f1831
ibuglienming <chinalienming@gmail.com>
2020-01-15 11:54:48 +0800

    debug in batch tar

--- batch_tar_res.sh
+++ batch_tar_res.sh
@@ -5,16 +5,18 @@ epochs=$3
 finalRootDir=~/en/FineLocator/expRes/final
 predictResDir=~/en/FineLocator/expRes/res
 
+cd $finalRootDir
 for((a=1;a<=10;a+=1))
 do
     for((b=1;b<=10-$a;b+=1))
     do
         r=$((10-$a-$b))
         if [[ $r -ge 0 ]]; then
-            tar -zcvf ${proj}_${dim}_${epochs}_${a}${b}${r}.tar ${finalRootDir}/${proj}/${proj}_*_${dim}_${epochs}_${a}${b}${r}
+            tar -zcvf ${proj}_${dim}_${epochs}_${a}${b}${r}.tar ${proj}/${proj}_*_${dim}_${epochs}_${a}${b}${r}
         fi
     done
 done
 
 tar -zcvf ${proj}_${dim}_${epochs}.tar  ${proj}_${dim}_${epochs}_*.tar
 rm ${proj}_${dim}_${epochs}_*.tar
+cd ~/en/FineLocator/ 
