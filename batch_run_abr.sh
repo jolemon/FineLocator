221aeecd5993009cb25a00f33d78ff0e254fadd3
jolemon <728385437@qq.com>
2020-12-14 20:10:51 +0800

    for merge

--- batch_run_abr.sh
+++ batch_run_abr.sh
@@ -33,11 +33,12 @@ codeVecAfterPoolingDir=${vecAfterPoolingDir}/code
 buggy_version_file=${queryExpansionDir}/batch_process/all_buggy_version
 
 PYTHON=python3.7
-word2vec_model_dimension=300
-word2vec_model_epochs=10
+word2vec_model_dimension=200
+word2vec_model_epochs=30
 proj=$1
 
-for proj_name in $proj   # "Time" "Mockito"  "Lang"  "Math"  "Closure" 
+source activate FineLocator
+for proj_name in $proj # "Time" "Mockito" "Math"  "Closure" "Lang" 
 do
     echo "handle project "${proj_name}"..."
     for proj_id in `ls ${allMethodsDir}/${proj_name}`
@@ -47,29 +48,29 @@ do
         echo "begin time:" ${begin_time}
         cd ${scriptRootDir}
 
-  #    echo "step 2 : use deeplearning4j(word2vec) to get vectors of bug reports and methods"
-  #    ./run_word2vec.sh ${deeplearning4jDir} ${queryExpansionDir} ${PYTHON} ${proj_id}\
-  #                      ${brAfterPTDir}/${proj_name} ${codeAfterPTDir}/${proj_name} \
-  #                      ${correspondAfterPTDir}/${proj_name} \
-  #                      ${brTfidfDir}/${proj_name} ${codeTfidfDir}/${proj_name} \
-  #                      ${brVecRootDir}/${proj_name}  ${codeVecRootDir}/${proj_name} \
-  #                      ${word2vec_model_dimension}  ${word2vec_model_epochs}
-  #    cd ${scriptRootDir}
+        echo "step 2 : use deeplearning4j(word2vec) to get vectors of bug reports and methods"
+        ./run_word2vec.sh ${deeplearning4jDir} ${queryExpansionDir} ${PYTHON} ${proj_id}\
+                          ${brAfterPTDir}/${proj_name} ${codeAfterPTDir}/${proj_name} \
+                          ${correspondAfterPTDir}/${proj_name} \
+                          ${brTfidfDir}/${proj_name} ${codeTfidfDir}/${proj_name} \
+                          ${brVecRootDir}/${proj_name}  ${codeVecRootDir}/${proj_name} \
+                          ${word2vec_model_dimension}  ${word2vec_model_epochs}
+        cd ${scriptRootDir}
 
-  #    echo "step 3 : Calculate semantic similarity for all methods"
-  #    ./ss.sh ${queryExpansionDir} ${codeVecRootDir}/${proj_name}  \
-  #            ${ssRootDir}/${proj_name} ${proj_id} ${word2vec_model_dimension} ${PYTHON} 
-  #    cd ${scriptRootDir}
+        echo "step 3 : Calculate semantic similarity for all methods"
+        ./ss.sh ${queryExpansionDir} ${codeVecRootDir}/${proj_name}  \
+                ${ssRootDir}/${proj_name} ${proj_id} ${word2vec_model_dimension} ${PYTHON} 
+        cd ${scriptRootDir}
 
-  #    echo "step 4 : Calculate temporal proximity for all methods"
-  #    ./tp.sh  ${queryExpansionDir} ${correspondAfterPTDir}/${proj_name} ${tpRootDir}/${proj_name} \
-  #             ${ssRootDir}/${proj_name} ${proj_id} ${PYTHON}
-  #    cd ${scriptRootDir}
+        echo "step 4 : Calculate temporal proximity for all methods"
+        ./tp.sh  ${queryExpansionDir} ${correspondAfterPTDir}/${proj_name} ${tpRootDir}/${proj_name} \
+                 ${ssRootDir}/${proj_name} ${proj_id} ${PYTHON}
+        cd ${scriptRootDir}
 
 
-        for((a=1;a<=10;a+=1))
+        for((a=3;a<=5;a+=1))
         do
-            for((b=0;b<=10-$a;b+=1))
+            for((b=1;b<10-$a;b+=1))
             do 
                 r=$((10-$a-$b))
                	if [[ $r -ge 0 ]]; then 
@@ -90,7 +91,5 @@ do
         end_time=$(date  "+%Y/%m/%d-%H:%M:%S")
         echo "begin time: "${begin_time}"  end time:" ${end_time}
    done
-
-   # break 
 done
-
+source deactivate FineLocator
