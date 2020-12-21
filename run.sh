7f50056f687d61f7e6f7728068fb476ea73334b4
jolemon <728385437@qq.com>
2020-12-21 19:35:35 +0800

    增加一段代码用于中断后从断点继续执行

--- run.sh
+++ run.sh
@@ -5,14 +5,14 @@
 # buggy_version_file=${queryExpansionDir}/batch_process/all_buggy_version
 
 PYTHON=python3.7
-word2vec_model_dimension=200
+word2vec_model_dimension=300
 word2vec_model_epochs=10
 alpha=0.8
 beta=0.1
 gamma=0.1
 
 source activate FineLocator
-for proj_name in "openjpa" # "org.aspectj" # "tomcat" "lucene-solr" "hibernate-orm" # "zookeeper"
+for proj_name in "org.aspectj" # "openjpa" # "tomcat" "lucene-solr" "hibernate-orm" # "zookeeper"
 # for proj_name in "Time"  "Mockito"  "Lang"  "Math"  "Closure" 
 do
     echo "handle project "${proj_name}"..."
@@ -24,37 +24,37 @@ do
         begin_time=$(date  "+%Y/%m/%d-%H:%M:%S")
         echo "begin time:" ${begin_time}
         cd ${scriptRootDir}
-        echo "step 1 : preprocessing for bug report and method"   
-        ./run_pt.sh ${ptDir} \
-                    ${bugReport4VectorDir}/${proj_name} \
-                    ${allMethodsDir}/${proj_name} \
-                    ${gitDir}  \
-                    ${proj_id} \
-                    ${brAfterPTDir}/${proj_name} \
-                    ${extractAfterPTDir}/${proj_name} \
-                    ${correspondAfterPTDir}/${proj_name} \
-                    ${codeAfterPTDir}/${proj_name} \
-                    ${buggy_version_file} \
-                    ${proj_name} \
-                    ${threads}
-        cd ${scriptRootDir}
+#         echo "step 1 : preprocessing for bug report and method"   
+#         ./run_pt.sh ${ptDir} \
+#                     ${bugReport4VectorDir}/${proj_name} \
+#                     ${allMethodsDir}/${proj_name} \
+#                     ${gitDir}  \
+#                     ${proj_id} \
+#                     ${brAfterPTDir}/${proj_name} \
+#                     ${extractAfterPTDir}/${proj_name} \
+#                     ${correspondAfterPTDir}/${proj_name} \
+#                     ${codeAfterPTDir}/${proj_name} \
+#                     ${buggy_version_file} \
+#                     ${proj_name} \
+#                     ${threads}
+#         cd ${scriptRootDir}
 
-        # echo "step 2 : use deeplearning4j(word2vec) to get vectors of bug reports and methods"
-        # ./run_word2vec.sh ${deeplearning4jDir} \
-        #                   ${queryExpansionDir} \
-        #                   ${PYTHON} \
-        #                   ${proj_id}\
-        #                   ${brAfterPTDir}/${proj_name} \
-        #                   ${codeAfterPTDir}/${proj_name} \
-        #                   ${correspondAfterPTDir}/${proj_name} \
-        #                   ${brTfidfDir}/${proj_name} \
-        #                   ${codeTfidfDir}/${proj_name} \
-        #                   ${brVecRootDir}/${proj_name} \
-        #                   ${codeVecRootDir}/${proj_name} \
-        #                   ${word2vec_model_dimension} \
-        #                   ${word2vec_model_epochs} \
-        #                   ${threads}
-        # cd ${scriptRootDir}
+        echo "step 2 : use deeplearning4j(word2vec) to get vectors of bug reports and methods"
+        ./run_word2vec.sh ${deeplearning4jDir} \
+                          ${queryExpansionDir} \
+                          ${PYTHON} \
+                          ${proj_id}\
+                          ${brAfterPTDir}/${proj_name} \
+                          ${codeAfterPTDir}/${proj_name} \
+                          ${correspondAfterPTDir}/${proj_name} \
+                          ${brTfidfDir}/${proj_name} \
+                          ${codeTfidfDir}/${proj_name} \
+                          ${brVecRootDir}/${proj_name} \
+                          ${codeVecRootDir}/${proj_name} \
+                          ${word2vec_model_dimension} \
+                          ${word2vec_model_epochs} \
+                          ${threads}
+        cd ${scriptRootDir}
 
         # echo "step 3 : Calculate semantic similarity for all methods"
         # ./ss.sh ${queryExpansionDir} \
